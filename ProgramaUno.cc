fa7c76d0614cddd664f8de981eda9af660c0df54
RobertoRomeroHernandez <31378324+RobertoRomeroHernandez@users.noreply.github.com>
2017-11-08 13:01:35 -0600

    08/11/17

--- ProgramaUno.cc
+++ ProgramaUno.cc
@@ -1,17 +1,21 @@
+//Examen: este programa si se da un número mayor a 12 dará el mismo número y si es menor se le suman 2 y dirá si es primo o no.
+//Edad 22 años 
+//Fecha de nacimiento: 8 de mayo de 1995  
+//Resultado del programa 22
+//Roberto Romero Hernández
+//Fecha de creación: 8 de noviembre de 2017
 #include <iostream>
 using namespace std;
 
-int isPrime(int X){
+int esPrimo(int X){
   int flag=0; 
  
   for(int i=2; i<= (X/2) ; i++){
     if(X%i==0){
       flag=1;
-      break;
     }
   }   
   return flag;
-
 }
 
 int main (){
@@ -22,28 +26,27 @@ int main (){
   cout << "Dame un número entero" << endl;
   cin >> p;
 
- while (S!=0){ 
-   cout << "ciclo S!=0, S " << S << ",p " << p << endl;
-   while(p<S){
-       cout << "ciclo p<S, S " < S << ",p " << p << endl;
+  while (S!=0){ 
+    //    cout << "S es: " << S << " ,p es: " << p << endl;
+    while(p<S){
+      //  cout << "ciclo p<S, S " << S << ",p " << p << endl;
 
-    while(isPrime(p)==1){
-      cout << "ciclo isPrime, S " << S << ",p " << p << endl;
-      cout << p << " no es primo  " << endl;
-      if((p%2)==0)
-	p=p+1;
-      p=p+2;
-      cout << "final ciclo isPrime, S " << S << ",p " << p << endl;
-    }  
+      while(esPrimo(p)==1){
+	//	cout << "ciclo esPrimo, S " << S << ",p " << p << endl;
+	cout << p << " no es primo  " << endl;
+	if((p%2)==0)
+	  p=p+1;
+	p=p+2;
+	//	cout << "final ciclo esPrimo, S " << S << ",p " << p << endl;
+      }  
     
-    S=S-p;
-    p=p+2
-    cout << "final ciclo p<S, S " << S << ",p " << p << endl;
-   }
-
-  S=S-1;
-  cout < "final ciclo S!=0, S " << S << ",p " << p << endl;
- }
+      S=S-p;
+      p=p+2;
+      //  cout << "final ciclo p<S, S " << S << ",p " << p << endl;
+    }
+    S=S-1;
+    //  cout << "final ciclo S!=0, S " << S << ",p " << p << endl;
+  }
   
   cout << " el valor de P es " << p << endl;
   return 0;
